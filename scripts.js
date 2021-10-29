ce4230c8b15da82f519b13625e52329b1c46ec20
zackzellto <zacharyzellto@gmail.com>
2021-10-29 09:23:19 -0600

    added delete button

--- scripts.js
+++ scripts.js
@@ -6,35 +6,38 @@ function showTaskInput(){
 
     const displayInput = document.getElementById("userInput").value;
     document.getElementById("userInput").value = "";
-    const newPara = document.createElement("p");
-    //newPara.setAttribute('id', '')
+    const createUL = document.createElement("UL");
     const toDoLine = document.createTextNode(displayInput);
-    newPara.appendChild(toDoLine);
-    document.getElementById("newToDoLine").appendChild(newPara);
-
+    createUL.appendChild(toDoLine);
+    document.getElementById("newToDoLine").appendChild(createUL);
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'X';
+    createUL.appendChild(deleteButton);
+    deleteButton.addEventListener('click', () => deleteToDo(deleteButton));
 }
+
+//prevent page from refreshing on submit, delete or check-off button click/keypress.
+deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showTaskInput();
+});
 
 //A user can delete an existing todo  (for todos that have been marked as completed)
-function deleteTaskButton(){
-    const toDoPara = document.querySelectorAll('p');
-    document.createElement("button");
-    document.getElementById("deleteTaskBtn").innerHTML = "";
-    toDoPara.appendChild("deleteTaskBtn");
+function deleteToDo(element) {
+    element.parentElement.remove();
 }
+
 
 //When a todo item is marked as completed the text of the todo should ce crossed out (using strikethrough font)
 function checkTaskButton(){
-    var strikeTask = displayInput.strike();
-    document.getElementById("userInput").innerHTML = strikeTask;
+    let strikeTask = displayInput.strike();
+    document.getElementById("userInput").textContent = strikeTask;
 }
 
-
-
-
-
-
-
-
-
-
-
+//Task displays when Enter key is pressed
+var input = document.getElementById('userInput');
+      window.addEventListener('keydown' , (enterKey) => {
+          if (enterKey.key === 'Enter'){
+              showTaskInput();
+          }
+        });
