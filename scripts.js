8cf855e34041bf5868add8fa4e82a0f5c97e43be
zackzellto <zacharyzellto@gmail.com>
2021-10-29 23:08:39 -0600

    added styling and some functionality

--- scripts.js
+++ scripts.js
@@ -1,43 +1,47 @@
-//A user can create a todo item by entering text into an input box and pressing a button
-
-
 //A list of created todo items are shown on the screen
 function showTaskInput(){
-
     const displayInput = document.getElementById("userInput").value;
-    document.getElementById("userInput").value = "";
     const createUL = document.createElement("UL");
     const toDoLine = document.createTextNode(displayInput);
-    createUL.appendChild(toDoLine);
-    document.getElementById("newToDoLine").appendChild(createUL);
     const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'X';
-    createUL.appendChild(deleteButton);
-    deleteButton.addEventListener('click', () => deleteToDo(deleteButton));
-}
+    const checkboxButton = document.createElement('button');
 
-//prevent page from refreshing on submit, delete or check-off button click/keypress.
-deleteButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    showTaskInput();
-});
-
-//A user can delete an existing todo  (for todos that have been marked as completed)
-function deleteToDo(element) {
-    element.parentElement.remove();
-}
-
-
-//When a todo item is marked as completed the text of the todo should ce crossed out (using strikethrough font)
-function checkTaskButton(){
-    let strikeTask = displayInput.strike();
-    document.getElementById("userInput").textContent = strikeTask;
+        document.getElementById("userInput").value = "";
+        createUL.appendChild(toDoLine);
+        document.getElementById("newToDoLine").appendChild(createUL);
+        deleteButton.setAttribute("id", "delete-btn")
+        createUL.appendChild(deleteButton);
+        deleteButton.addEventListener('click', () => deleteToDo(deleteButton));
+        checkboxButton.setAttribute("class", "task-checkbox");
+        createUL.appendChild(checkboxButton);
 }
 
 //Task displays when Enter key is pressed
-var input = document.getElementById('userInput');
+let input = document.getElementById('userInput');
       window.addEventListener('keydown' , (enterKey) => {
           if (enterKey.key === 'Enter'){
               showTaskInput();
           }
         });
+
+//clear all tasks from list w/ button click.
+
+const clearTaskButton = document.getElementById('clear-to-do-list');
+        createUL.appendChild(clearTaskButton);
+        clearTaskButton.addEventListener('click', () => clearTasks(clearTaskButton))
+
+function clearTasks(e){
+    e.document.getElementById("newToDoLine").appendChild(createUL).value = '';
+}
+
+//A user can delete an existing todo  (for todos that have been marked as completed)
+function deleteToDo(e) {
+    e.parentElement.remove();
+}
+
+//When a todo item is marked as completed the text of the todo should ce crossed out (using strikethrough font)
+
+
+
+
+
